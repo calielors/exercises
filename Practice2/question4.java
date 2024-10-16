59419b699a6669e0a39e4b645688d2e5e283f08f
calielors <caliel_ors@hotmail.com>
2024-10-15 23:25:11 -0300

    update practice2

---
+++ Practice2/question4.java
@@ -0,0 +1,11 @@
+package Practice2;
+import java.util.Scanner;
+public class question4{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+            for (int i = 1; i <= n; i++) {
+                System.out.printf("%d ", i);
+            }
+    }
+}
---
+++ Practice2/question5.java
@@ -0,0 +1,11 @@
+package Practice2;
+import java.util.Scanner;
+public class question5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+                for (int i = 1; i <= n;) {
+                    System.out.printf("%d ", n); n--;
+        }
+    }
+}
